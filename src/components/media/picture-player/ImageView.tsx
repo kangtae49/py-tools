86289ad9b67822d465ddf12da0e77421852dce89
kangtae49 <kangtae49@gmail.com>
2025-10-21 20:57:38 +0900

    image opacity

--- src/components/media/picture-player/ImageView.tsx
+++ src/components/media/picture-player/ImageView.tsx
@@ -1,24 +1,34 @@
 import {usePictureStore} from "@/components/media/picture-player/pictureStore.ts";
 import {getFilename, srcLocal} from "@/components/utils.ts";
+import {useEffect, useState} from "react";
 
 interface Prop {
   width: number
   height: number
 }
 function ImageView({width, height}: Prop) {
+  const [opacity, setOpacity] = useState(0);
   const {
     setMediaRef,
     setViewType,
     setting,
   } = usePictureStore()
 
+  useEffect(() => {
+    setOpacity(0);
+  }, [setting.mediaPath])
+
   return (
-    <div className="picture-image" style={{width, height}}
+    <div className="picture-image"
+         style={
+            {width, height, opacity}
+         }
       ref={setMediaRef}
+         onLoad={() => setOpacity(1)}
          onClick={() => setViewType('grid')}
     >
       {setting.mediaPath && <img src={srcLocal(setting.mediaPath)}
-           // loading="lazy"
+           loading="lazy"
            alt={getFilename(setting.mediaPath)}
       />}
     </div>
